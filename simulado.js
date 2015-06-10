9d8e1854de038431efe5e5dbd45eaa5267e59456
Lawrence Dabir-Alai <lawrence.dabir@gmail.com>
2015-06-10 02:01:32 +0100

    Initial commit, don't expect much

---
+++ simulado.js
@@ -0,0 +1,45 @@
+var app = require('express')();
+var mocks = [];
+
+var Simulado = function() {
+    app.get('/', function(_, res) {
+        res.send("Simulado running.."); 
+    });
+    app.all('*', function(req, res) {
+        findMock(req, function(index) {
+            if(index != null && index >= 0) {
+                res.status(mocks[index].status).send(mocks[index].response);
+            } else {
+                res.status(404).send({});
+            }
+        });
+    });
+    app.listen(7000, function() {
+        
+    });
+};
+
+Simulado.prototype.mock = function(opts, callback) {
+    var baseMock = {
+        path: opts.path || '',
+        status: opts.status || 200,
+        response: opts.response || {}
+    }
+    findMock(opts, function(index) {
+        if(index >= 0)
+            mocks.splice(index, 1);
+        mocks.push(baseMock);
+        callback();
+    });
+}
+
+function findMock(req, callback) {
+    var index = mocks.map(function(m) { return m.path }).indexOf(req.path);
+    if(index >= 0) {
+        callback(index)
+    } else {
+        callback(null)
+    }
+}
+
+module.exports = new Simulado();
---
+++ test/test.js
@@ -0,0 +1,55 @@
+var Simulado = require('../simulado.js');
+var chai = require('chai').should();
+var superagent = require('superagent');
+
+describe('Simulado', function() {
+    it('should start up a webserver', function(done) {
+        superagent.get('http://localhost:7000/').end(function(_, res) {
+            res.status.should.equal(200)
+            res.text.should.equal("Simulado running..")
+            done()
+        });
+    });
+    it('should respond with a 404 on any pages which are not specifically mocked', function(done) {
+        superagent.get('http://localhost:7000/not-mocked').end(function(_, res) {
+            res.status.should.equal(404)
+            done()
+        });
+    });
+    it('should send back empty json response if no response or status is provided', function(done) {
+        Simulado.mock({
+            path: '/test'
+        }, function() {
+            superagent.get('http://localhost:7000/test').end(function(_, res) {
+                res.status.should.equal(200)
+                res.text.should.equal('{}')
+                done()
+            });
+        });
+    });
+    it('should respond with a status code with an empty json when only status is mocked', function(done) {
+        Simulado.mock({
+            path: '/test',
+            status: 401
+        }, function() {
+            superagent.get('http://localhost:7000/test').end(function(_, res) {
+                res.status.should.equal(401)
+                res.text.should.equal('{}')
+                done()
+            });
+        });
+    });
+    it('should respond with a fully mocked response (status & text)', function(done) {
+        Simulado.mock({
+            path: '/test',
+            status: 401,
+            response: "401 Unauthorised"
+        }, function() {
+            superagent.get('http://localhost:7000/test').end(function(_, res) {
+                res.status.should.equal(401)
+                res.text.should.equal("401 Unauthorised")
+                done()
+            });
+        });
+    });
+});
