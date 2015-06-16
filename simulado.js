5cb1a5c73f4c45c83fcf258edb1feb8430a8c647
Alex Foster <alexjfno1@gmail.com>
2015-06-16 18:56:11 +0100

    AF: Extracting server into it's own file

--- simulado.js
+++ simulado.js
@@ -1,36 +1,8 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var Server = require("./lib/server.js");
 var responseStore = require('./lib/responseStore');
 var requestStore = require('./lib/requestStore');
 
-app.use(cors());
-app.use(bodyParser.json());
-
 var Simulado = function() {
-    app.get('/', function(_, res) {
-        res.send("Simulado running..");
-    });
-
-    app.get('/inspect', function(_, res) {
-      res.send(responseStore.getAll());
-    });
-
-    app.all('*', function(req, res) {
-        responseStore.find(req, function(mock) {
-            if(mock) {
-                requestStore.add(req);
-                for(var header in mock.headers) {
-                    res.header(header, mock.headers[header]);
-                }
-                res.status(mock.status).send(mock.response);
-            } else {
-                res.status(404).send({});
-            }
-        });
-    });
-
-    this.server = app.listen(7000)
 
     this.mock = responseStore.add;
 
@@ -40,6 +12,8 @@ var Simulado = function() {
         responseStore.reset();
         requestStore.reset();
     }
+
+    new Server().start(7000);
 
 };
 
