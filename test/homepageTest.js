0482688ec29606663441a284a6fb0d910ebdedcf
Jakub Witkiewicz <krow10@gmail.com>
2015-12-28 15:28:34 +0000

    Homepage support for multiple responses and conditional request body

--- test/homepageTest.js
+++ test/homepageTest.js
@@ -89,6 +89,42 @@ describe('Simulado homepage', function () {
 
     });
   });
+  
+  
+  describe('when there are multiple possible responses for an http method & endpoint', function() {
+    
+    beforeEach(function (done){    
+      Simulado.mock({ path: '/test', response: "A" }, function() {
+          Simulado.mock({ path: '/test', response: "B", conditionalRequestBody: { "conditionalRequest": 1 } }, function() {
+            Simulado.mock({ path: '/test', response: "C", conditionalRequestBody: { "conditionalRequest": 2 }  }, done);    
+          });
+        });                     
+    });
+    
+    it('should show how many possible responses are mocked for a particular endpoint', function (done) {
+        browser.visit('http://localhost:7000/', function () {
+          browser.assert.text('.responses-info', '3 possible responses on this http method & endpoint');
+          done();
+        });
+    });
+    
+    it('should show the conditional request body for responses that have it', function (done) {
+        browser.visit('http://localhost:7000/', function () {
+          browser.assert.text('.conditional-request-body', 'Only responds to: { "conditionalRequest": 1 } Only responds to: { "conditionalRequest": 2 }');
+          done();
+        });
+    });    
+    
+    it('should show mocked response bodies', function (done) {
+        browser.visit('http://localhost:7000/', function () {
+          browser.assert.text('.response-body', 'Response body: "A" Response body: "B" Response body: "C"');
+          done();
+        });
+    });
+    
+  });
+  
+  
 
 
 
