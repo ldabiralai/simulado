112aec851eb4c6f85ee4c365f663adc2bc80cb0b
Alex Foster <alexjfno1@gmail.com>
2017-10-06 10:43:13 +0100

    Check if src dir has been formatted

--- dangerfile.js
+++ dangerfile.js
@@ -1,2 +1,14 @@
-import { message, danger } from "danger"
-message(":tada:, this worked @" + danger.github.pr.user.login)
+import { message, danger } from "danger";
+import prettier from 'prettier';
+
+const srcDir = `${__dirname}/src/**/*.js`;
+const options = {
+  singleQuote: true,
+  printWidth: 100
+};
+
+if (prettier.check(srcDir, options)) {
+  message(':tada: Your code is formatted correctly');
+} else {
+  warn('You haven\'t formated the code using prettier. Please run `npm run format` before merging the PR');
+}
